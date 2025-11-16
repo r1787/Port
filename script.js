8be431222a1fa5518b872bf934353e1c8a380b2a
rxtobi01-hash <rxtobi01@gmail.com>
2025-11-16 23:49:51 +0100

    update index.html

--- script.js
+++ script.js
@@ -42,23 +42,7 @@ window.addEventListener('scroll', () => {
 });
 
 // Form submission
-const contactForm = document.querySelector('.contact-form');
-if (contactForm) {
-    contactForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        
-        // Get form data
-        const formData = new FormData(contactForm);
-        const data = Object.fromEntries(formData);
-        
-        // Here you would typically send the data to a server
-        console.log('Form submitted:', data);
-        
-        // Show success message
-        alert('Thank you for your message! I\'ll get back to you soon.');
-        contactForm.reset();
-    });
-}
+
 
 // Animation on scroll
 const observerOptions = {
